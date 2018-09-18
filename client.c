3cc138ea3c0d157de9d033028a9ea37825438b90
Daniel Limanowski <dlimanow@iastate.edu>
2018-09-18 08:30:42 -0500

    add list of servers

--- client.c
+++ client.c
@@ -13,31 +13,35 @@ int main(int argc, char const *argv[])
         struct sockaddr_in serv_addr;
         char *message = "uptime";
         char buffer[1024] = {0};
-
+        const char *servers[] = {"127.0.0.1"}; // add any server IP to list
+        int num_servers = sizeof(servers) / sizeof(servers[0]); 
+    
         if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) { 
-            printf("Failed to create socket."); 
+            printf("Failed to create socket.\n"); 
             return -1;
         }
 
         memset(&serv_addr, '0', sizeof(serv_addr)); // allocate memory to store address
-
-        serv_addr.sin_family = AF_INET; // IPv4
         serv_addr.sin_port = htons(PORT); // network byte order conversion
+        serv_addr.sin_family = AF_INET; // IPv4
 
-        // convert IP address from text to binary
-        if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-                printf("Invalid address provided.");
-                return -1;
+        int i = 0;
+        for (i = 0; i < num_servers; i++) {
+            // convert IP address from text to binary
+            if (inet_pton(AF_INET, servers[i], &serv_addr.sin_addr) <= 0) {
+                    printf("Invalid address provided.\n");
+                    return -1;
+            }
+
+            if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+                    printf("Connection Failed.\n");
+                    return -1;
+            }
+
+            send(sock, message, strlen(message), 0);
+            printf("Uptime request sent\n");
+            valread = read(sock, buffer, 1024);
+            printf("[SERVER %s] %s\n", servers[i], buffer);
         }
-
-        if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-                printf("\nConnection Failed \n");
-                return -1;
-        }
-
-        send(sock, message, strlen(message), 0);
-        printf("Uptime request sent\n");
-        valread = read(sock, buffer, 1024);
-        printf("SERVER RESPONSE: %s\n", buffer);
         return 0;
 }
